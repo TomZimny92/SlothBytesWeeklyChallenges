1c5fbac66f01752fa767f5b60e279d428557625b
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-06-29 20:38:29 -0400

    no time to die

--- SlothSuite.ts
+++ SlothSuite.ts
@@ -5,4 +5,14 @@ import * as commander from 'commander'
 const program = new commander.Command()
 program.version('1.0.0')
 
+program
+	.command('say-hello')
+	.description('Prints a friendly greeting')
+	.action(() => {
+		console.log('Hello, world')
+	})
+
+
 program.parse(process.argv)
+
+
