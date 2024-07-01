27dfd6d67165301d17e1f013a655a5716f21e71d
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-06-30 20:32:32 -0400

    learning

--- SlothSuite.js
+++ SlothSuite.js
@@ -1,1 +1,13 @@
-console.log("Hello, node.js");
+import * as commander from 'commander'
+
+const program = new commander.Command()
+program.version('1.0.0')
+
+program
+	.command('say-hello')
+	.description('Prints a friendly greeting')
+	.action(() => {
+		console.log('Hello world')
+	})
+
+program.parse(process.argv)
