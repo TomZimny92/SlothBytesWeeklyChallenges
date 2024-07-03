07b7954286cc665b84da9b96ba192c5cb0a15724
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-07-02 20:41:17 -0400

    new sloth bytes

---
+++ BinarySearch/BinarySearch.js
@@ -0,0 +1,17 @@
+// Binary Search - Sloth Bytes 7/2/2024
+//
+// Given a sorted array of integers and a target integer, find the first occurrence
+// of the target and return its index.
+//
+// Return -1 if the target is not in the array.
+//
+//
+
+function findTarget(arr, target){
+	let begin = 0
+	let end = arr.length - 1
+
+	while (begin <= end){
+
+	}
+}
