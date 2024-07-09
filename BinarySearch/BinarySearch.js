f3e870da39a09b122e725c59a8962866625c1394
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-07-08 20:27:05 -0400

    binary search

--- BinarySearch/BinarySearch.js
+++ BinarySearch/BinarySearch.js
@@ -7,11 +7,29 @@
 //
 //
 
+let arr = [1, 3, 5, 7, 9, 11, 13, 15, 17] 
+
 function findTarget(arr, target){
 	let begin = 0
 	let end = arr.length - 1
 
 	while (begin <= end){
-
+		let mid = Math.floor((begin + end) / 2)
+		if (arr[mid] == target){
+			return mid
+		}
+		else if (arr[mid] < target){
+			console.log(`arr[mid]: ${arr[mid]}`)
+			console.log(`mid + 1: ${mid + 1}`)
+			begin = mid + 1
+		}
+		else {
+			console.log(`arr[mid]: ${arr[mid]}`)
+			console.log(`mid + 1: ${mid + 1}`)
+			end = mid - 1
+		}
 	}
+	return -1
 }
+
+console.log(findTarget(arr, 11))
