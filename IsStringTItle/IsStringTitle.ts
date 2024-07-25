87136b85fd91b9cb604f4c45df8753e68b49a069
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-07-25 19:41:01 -0400

    sloth bytes

---
+++ IsStringTItle/IsStringTitle.ts
@@ -0,0 +1,17 @@
+// Sloth Bytes Challenge - July 23, 2024
+//
+// Check if a string is formatted as a title. 
+// A title string is when every word of the string start with an upper case letter
+//
+// 
+function IsStringTitle(input: string): boolean {
+	const words: string[] = input.str(' ')
+	let isTitle: boolean = true
+	for (let i: number = 0; i < words.length; ++i){
+		let word: string = words[i]
+		if (word[0] !== word[0].toUpperCase()){
+			isTitle = false	
+		}
+	}
+	return isTitle
+}
