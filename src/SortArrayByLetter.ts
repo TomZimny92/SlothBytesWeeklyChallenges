eca2653bd6368c67eea2ca966dbec9709385a683
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-07-31 20:33:35 -0400

    new sloth bytes

---
+++ src/SortArrayByLetter.ts
@@ -0,0 +1,18 @@
+// Sloth Bytes Weekly Challenge - 7/30/2024
+//
+// Sort by the Letters:
+// Write a function that sorts each string in a list by the letter in alphabetic ascending order (a-z)
+//
+//
+
+let arr1: string[] = ['932c', '832u32', '2344b']
+let arr2: string[] = ['99a', '78b', 'c2345', '11d']
+let arr3: string[] = ['572z', '5y5', '304q2']
+
+function SortByLetter(input: string[]): void {
+	let output: string[] = []
+
+	for(let i: number = 0; i < input.length; ++i){
+
+	}
+}
