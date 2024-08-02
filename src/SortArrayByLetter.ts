b5e109df1de6e65ef265975e83c02a4cd20de867
TomZimny92 <64618507+TomZimny92@users.noreply.github.com>
2024-08-01 20:45:51 -0400

    first try

--- src/SortArrayByLetter.ts
+++ src/SortArrayByLetter.ts
@@ -3,7 +3,9 @@
 // Sort by the Letters:
 // Write a function that sorts each string in a list by the letter in alphabetic ascending order (a-z)
 //
-//
+// Each string will only have one (lowercase) letter
+// If given an empty list, return an empty list
+
 
 let arr1: string[] = ['932c', '832u32', '2344b']
 let arr2: string[] = ['99a', '78b', 'c2345', '11d']
@@ -11,8 +13,27 @@ let arr3: string[] = ['572z', '5y5', '304q2']
 
 function SortByLetter(input: string[]): void {
 	let output: string[] = []
-
-	for(let i: number = 0; i < input.length; ++i){
-
+	if(input.length ===) {
+		console.log(output)
 	}
+	for(let i: number = 0; i < input.length; ++i){
+		let dictionary = new Map<string, string[]>()
+//		input[i].forEach((c) => {
+//			if (c)
+
+		//})
+		for(let j: number = 0; j < input[i].length; ++j){
+			if (input[i].charCodeAt(j) >== 97 || input[i].charCodeAt(j) <== 122){
+				dictionary.set(input[i].charAt(j), input[i])
+			}
+		}
+	}
+	dictionary = new Map([...dictionary.entries()].sort())
+	
+	dictionary.forEach((key: string, value: string[]) => {
+		output.push(value)
+	})	
+
+	console.log(output.toString())
+
 }
